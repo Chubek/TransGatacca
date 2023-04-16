f98c7dbc0783a4f601a9dd5c44ac2728aafb4e76
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-04-16 14:52:31 +0330

    new encoding, added hook, print ot file, etc

--- encode/encode_tables.py
+++ encode/encode_tables.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 #############################################################################
 # Copyright (c) 2023 Chubak Bidpaa                                          #
 # Permission is hereby granted, free of charge, to any person obtaining     #
@@ -20,6 +22,7 @@
 
 # Encode codon tables for a 64bit-width register or memory address
 # Please read ENCODING.md for more info
+
 
 GENETIC_TABLE = {
     "1": {
@@ -806,25 +809,113 @@ GENETIC_TABLE = {
     }
 }
 
-final_encoded_peps = {}
-final_encoded_nucs = {}
-encode_single_nuc = lambda x: 3 & (x >> 1)
-for genetic_code, table in GENETIC_TABLE.items():
-    final_encoded_nucs[genetic_code] = [0] * 64
-    final_encoded_peps[genetic_code] = [0] * 26
 
-    for peptide, nucleotide_triplets in table.items():        
-        for i, trip in enumerate(nucleotide_triplets):
-            indx = (encode_single_nuc(trip[0]) << 4) & 0xff
-            indx |= (encode_single_nuc(trip[1]) << 2) & 0xff
-            indx |= encode_single_nuc(trip[2])
-            final_encoded_nucs[genetic_code][indx] = peptide
-            final_encoded_peps[genetic_code][ord(peptide) - 65] |= (indx << ((10 - i) * 6))
+__METHOD_DOC_COMM = {
+    "NucleotideEncodingMethod": "3 & (<nucleotide> >> 1)",
+    "NucleotideEncodingMapping": {
+        "A": 0,
+        "C": 1,
+        "T": 2,
+        "G": 3,
+    },
+    "NucleotidePackedMaxNbits": 7,
+    "NucleotidePackedMaxValue": 63,
+    "CorrespondingFile": "Github/chubek/TransGatacca/encoding/encode_tables.py",
+}
 
-        final_encoded_peps[genetic_code][ord(peptide) - 65] <<= 4
-        final_encoded_peps[genetic_code][ord(peptide) - 65] |= len(nucleotide_triplets)
+__METHOD_DOC_NUC = {
+    **__METHOD_DOC_COMM,
+    "TripletPackingMethod": "(<nucletide_1> << 4) | (<nucletide_2> << 2) | <nucletide_3>",
+    "IndexingMethod": "ARRAY[<packed_nucleotide>] = <peptide>",
+    "PackedTripletExample": "MSB=01 B1=11 LSB=00 -> CGA"
+}
+__METHOD_DOC_PEP = {
+    **__METHOD_DOC_COMM,
+    "CodonPackingMethod": "foreach Nth <packed_triplet>: packed |= <packed_triplet> << (<pack_offset> inc by 6)",
+    "PackOffsetInitial": 4,
+    "PackOffsetMax": 54,
+    "PackMaxNbits": 54,
+    "MaxNumberOfCodons": 9,
+    "MinNumberOfCodons": 1,
+    "MaxNumberOfCodonsNBits": 4,
+    "CodonsNumEncodingMethod": "<packed_codons> | <number_of_codons>",
+    "IndexingMethod": "ARRAY[<ASCII_VAL(peptide) - 65>] = <packed_codons>",
+    "PackedCodonsExample": "MSB=001110 B1=111100 LSB=0010 -> (AGT, GGA, len=2)",
+}
 
 
-import json
-with open("encoded_codons_revtable.json", "w") as fw:
-    fw.write(json.dumps(final_encoded_peps))
+def execfn(fn: type):
+    nucs, peps = fn()
+    globals()["__ENCODED__"] = (nucs, peps)
+    return fn
+
+
+@execfn
+def encode_gtable(gtable=GENETIC_TABLE) -> tuple[dict[str, list[int]], dict[str, list[int]]]:
+    final_encoded_peps = {}
+    final_encoded_nucs = {}
+
+    def enc_nuc(nuc): return 3 & (nuc >> 1)
+    iss = []
+
+    for genetic_code, table in gtable.items():
+        final_encoded_nucs[genetic_code] = [0] * 64
+        final_encoded_peps[genetic_code] = [0] * 26
+        for peptide, nucleotide_triplets in table.items():
+            peptide_idx = ord(peptide) - 65
+            shl_amount = 4
+            for i, trip in enumerate(nucleotide_triplets):
+                indx = (enc_nuc(trip[0]) << 4) & 0xff
+                indx |= (enc_nuc(trip[1]) << 2) & 0xff
+                indx |= enc_nuc(trip[2])
+                final_encoded_nucs[genetic_code][indx] = ord(peptide)
+                final_encoded_peps[genetic_code][peptide_idx] |= (
+                    indx << shl_amount)
+                shl_amount += 6
+
+            final_encoded_peps[genetic_code][peptide_idx] |= len(
+                nucleotide_triplets)
+
+    return final_encoded_nucs, final_encoded_peps
+
+
+if __name__ == "__main__":
+    encd_nucs, encd_peps = globals()["__ENCODED__"]
+
+    import sys
+    import json
+    if len(sys.argv) < 4:
+        print("Arg number must be 3")
+        exit(1)
+    cmd, file_nuc, file_pep = sys.argv[1:4]
+    fnuc = open(file_nuc, "w")
+    fpep = open(file_pep, "w")
+    if cmd.startswith("c"):
+        for i in range(37):
+            print("{", file=fnuc)
+            print("{", file=fpep)
+            if str(i) in encd_nucs:
+                gtable_enc = encd_nucs[str(i)]
+                print(
+                    "{ ", ", ".join([map(str, gtable_enc)]), " },", file=fnuc)
+                gtable_enc = encd_peps[str(i)]
+                print(
+                    "{ ", ", ".join([map(str, gtable_enc)]), " },", file=fpep)
+            else:
+                print("ARR_ZERO_64,", file=fnuc)
+                print("ARR_ZERO_26,", file=fpep)
+            print("}", file=fnuc)
+            print("}", file=fpep)
+    elif cmd.startswith("j"):
+        encd_nucs["Method"] = __METHOD_DOC_NUC
+        encd_peps["Method"] = __METHOD_DOC_PEP
+        fnuc.write(json.dumps(encd_nucs, indent=4))
+        fpep.write(json.dumps(encd_peps, indent=4))
+    else:
+        print("Unknown command: " + cmd)
+        fnuc.close()
+        fpep.close()
+        exit(1)
+
+    fnuc.close()
+    fpep.close()
